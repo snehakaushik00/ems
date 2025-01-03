4d5db8ec7af951781e50ecc2db4bc8660e1c886b
sneha-kaushik <kaushik.sneha273@gmail.com>
2025-01-04 00:30:52 +0530

    modified:   src/App.jsx 	modified:   src/components/Dashboard/AdminDashboard.jsx 	new file:   src/components/other/AllTask.jsx 	new file:   src/components/other/CreateTask.jsx 	modified:   src/context/AuthContext.jsx 	modified:   src/context/TaskContext.jsx 	modified:   src/main.jsx

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,31 +1,14 @@
 import React from 'react'
 import Header from '../other/header'
+import CreateTask from '../other/CreateTask'
+import AllTask from '../other/AllTask'
   
 const AdminDashboard = () => {
   return (
-    <div className='h-screen w-full p-10'>
+    <div className='h-screen w-full p-7'>
       <Header/>
-
-      <div>
-        <form className='flex items-start justify-between'>
-        <div>
-         <h3>Task Title</h3></div>
-          <input  type="text" placeholder='Make a ui design'/>
-         <div>
-          <h3>Description</h3>
-         <textarea name="" id="" cols="30" rows="10"></textarea></div>
-         <div>
-          <h3>Date</h3>
-          <input type="date" /></div>
-          <div>
-            <h3>Asign to</h3>
-          <input type="text" placeholder='employee name' /></div>
-          <div>
-            <h3>Category</h3>
-          <input type="text" placeholder='design, dev, etc'/></div>
-          <button>Create Task</button>
-        </form>
-      </div>
+     <CreateTask/>
+     <AllTask/>
     </div>
   )
 }
